940b51455bd770088e1c58799e49274900e913cb
Osni Carfi <osnicarfi@hotmail.com>
2014-06-07 20:44:27 -0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Amitie::Application.config.secret_key_base = 'ae50cf5aca2367209c9741f0075c8fd9d30fa1baf223c87d01394505fd72d9f9b80717f6bd12b5cfcf145849d3b00c97d40a54841d5689b5304c376a35504399'
